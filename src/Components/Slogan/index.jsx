09f3f91e50f662add57709d240774a2e4c426e1b
accauntforwork <accauntforwork01@gmail.com>
2024-05-03 09:16:14 +0500

    change slogan size

--- src/Components/Slogan/index.jsx
+++ src/Components/Slogan/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Slogan() {
   return (
     <div className="mb-4">
-      <h1 className="text-6xl font-semibold leading-[72px] letter-[-0.5px] text-center text-[#87CEEB]">
+      <h1 className="text-4xl font-semibold leading-[72px] letter-[-0.5px] text-center text-[#87CEEB]">
         CRYPTOFOLIO WATCH LIST
       </h1>
       <p className="text-center text-sm capitalize font-medium text-[#A9A9A9]">
